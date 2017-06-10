b5c95d65223e9e833d3bc5e425e4b6271c5827d1
Trishita Tiwari <trtiwari@bu.edu>
2017-06-10 09:36:20 +0000

    added new css files to make (almost) everything mobile friendly

--- resume/resumejs.js
+++ resume/resumejs.js
@@ -23,10 +23,10 @@ var main = function () {
     addbars(['HTML','CSS','Javascsript/JQuery','Django'],[1,0.8,0.5,0.25]);
    }
    else if (text === 'Languages') {
-    addbars(['Python','Java','C/C++'],[0.85,0.65,0.85]);
+    addbars(['Python','Java','C/C++','HTML','CSS','Javascsript/JQuery'],[0.85,0.65,0.85,1,0.8,0.5]);
    }
    else if (text === 'Cyber Sec') {
-   	addbars(['Web','Reverse Eng','Networks'],[0.65,0.25,0.75]);
+   	addbars(['Web','Reversing','Networks'],[0.65,0.25,0.75]);
    }
    else {
 	addbars(['Android Dev','InfluxDB','Algorithms','OS'],[0.65,0.75,0.75,0.25]);
